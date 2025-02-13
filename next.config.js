857fb5d2fd61a194424f42b58aed661e3b04d0c8
hozeronder <hozeronder@gmail.com>
2025-02-13 15:26:47 +0300

    sadf

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true
-  },
-  // Diğer ayarlar...
+  reactStrictMode: true,
+  swcMinify: true,
   async rewrites() {
     return [
       {
