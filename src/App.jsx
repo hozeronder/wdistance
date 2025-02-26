431869215c63b4bbf923b61db1d268fa2512a2f6
hozeronder <hozeronder@gmail.com>
2025-02-26 12:33:29 +0300

    frontendyontemi24

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import './App.css'
-import IsochroneMap from './IsochroneMap';
 
 function App() {
   return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ width: '50%' }}>
-        <IsochroneMap />
-      </div>
-    </div>
+      <IsochroneMap/>
   )
 }
 
