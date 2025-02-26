9abb0d711426373daf04d879309064e1f379d2ac
hozeronder <hozeronder@gmail.com>
2025-02-26 12:24:05 +0300

    frontendyontemi24

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import './App.css'
 import IsochroneMap from './IsochroneMap';
-import CircleMap from './CircleMap';
 
 function App() {
   return (
     <div style={{ display: 'flex' }}>
       <div style={{ width: '50%' }}>
         <IsochroneMap />
-      </div>
-      <div style={{ width: '50%' }}>
-        <CircleMap />
       </div>
     </div>
   )
